44d02a67b184dd5ff841f00f412dfc05eccc6472
TomRight <zxvc@rambler.ru>
2022-06-02 08:39:28 +0500

    испытаение

---
+++ numberLetDel/www/script.js
@@ -0,0 +1,9 @@
+var inter = 1;
+while (inter > 0) {
+  const input = prompt();
+  var input1 = input.toString();
+  input1 = input1.replace(" ", "");
+  input2 = input1.replace("-", "");
+  console.log(input2);
+  input = " ";
+}
---
+++ randobgenerator/wwww/scrypt.js
@@ -0,0 +1,40 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+randomize.addEventListener('click', result);
+
+var storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+var insertX = ['Willy', 'the', 'Goblin',
+'Big', 'Daddy',
+    'Father', 'Christmas'];
+
+var insertY = ['the', 'soup', 'kitchen',
+    'Disneyland',
+    'the', 'White', 'House'];
+var insertZ = ['spontaneously', 'combusted',
+    'melted', 'into', 'a', 'puddle', 'on', 'the', 'sidewalk',
+    'turned', 'into', 'a', 'slug', 'and', 'crawled', 'away'];
+
+
+function result() {
+    var newStory = storyText;
+  
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
